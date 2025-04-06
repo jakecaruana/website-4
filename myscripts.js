f216902d1e222900f582b768bca624f9f71fbc0e
jakecaruana <jake.caruana.67@gmail.com>
2025-04-06 19:57:24 +0200

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,28 @@
+function moreaboutme() {
+   let text = document.getElementById("moreaboutme");
+   if (text.style.display === "none") {
+       text.style.display = "block";
+   } else {
+       text.style.display = "none";
+   }
+}
+
+function viewmore() {
+    let text = document.getElementById("viewmore");
+    if (text.style.display === "none") {
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+ }
+ 
+ function supriseme() {
+    let text = document.getElementById("supriseme");
+    if (text.style.display === "none") {
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+ }
+
+ 
